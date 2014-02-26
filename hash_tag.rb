4e7aad8fb04ce3d8c6964f094117aab904298fa3
steve <stevedanko1@gmail.com>
2014-02-26 00:36:01 -0500

    refactored a bit

--- hash_tag.rb
+++ hash_tag.rb
@@ -1,19 +1,15 @@
-require 'pry'
-
 def get_hashtags(post)
   @hashtag_words = [] if @hashtag_words.nil?
   post.split(" ").each do |word|
     word = word.split('')
-    if word[0] == "#"
-      if word[1] == "#"
-        word = word.join
-        word.slice!(0)
-        get_hashtags(word)
-      elsif word[1] =~ /[a-zA-Z]/ && word.count("#") == 1
-        word = word.join("")
-        word.slice!(0)
-        @hashtag_words << word
-      end
+    if word[0] == "#" && word[1] == "#"
+      word = word.join
+      word.slice!(0)
+      get_hashtags(word)
+    elsif word[1] =~ /[a-zA-Z]/ && word.count("#") == 1 && word[0] == "#"
+      word = word.join
+      word.slice!(0)
+      @hashtag_words << word
     end
   end
   @hashtag_words
